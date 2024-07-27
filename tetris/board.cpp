b0355b6f966a37d7469270bc5d7e056dc7d144d6
Boc7777 <111458718+Boc7777@users.noreply.github.com>
2024-07-27 15:16:10 +0200

    Reapply "game_over screen and fix some thg"
    This reverts commit cf430d3000728dbbb5770d1ca72a9b8964b9837a.

--- tetris/board.cpp
+++ tetris/board.cpp
@@ -5,22 +5,22 @@
 
 
 
-Board::Board(float weight, float height, float _cell_size, RenderWindow* _window) {
-	/*shape.setSize(Vector2f(weight, height));
-	shape.setFillColor(Color(128, 128, 128));
-	shape.setOutlineThickness(4.f);
-	shape.setOutlineColor(Color::White);*/
-
-	texture_board.loadFromFile("background.png");
+Board::Board(float _cell_size, RenderWindow* _window) {
+	
+	texture_board.loadFromFile("Graphics\\background.png");
 	shape.setTexture(texture_board);
 
-	texture_rightPanel.loadFromFile("RightPanel.png");
+	texture_rightPanel.loadFromFile("Graphics\\RightPanel.png");
 	rightPanel.setTexture(texture_rightPanel);
+
+	texture_gameover.loadFromFile("Graphics\\GameOver.png");
+	gameover.setTexture(texture_gameover);
+	gameover.setOrigin(gameover.getLocalBounds().width / 2, gameover.getLocalBounds().height / 2);
 	
 
 	cell_size = _cell_size;
 	window = _window;
-
+	level = 1;
 	
 	font.loadFromFile("czcionka.ttf");
 
@@ -33,14 +33,13 @@ Board::Board(float weight, float height, float _cell_size, RenderWindow* _window
 	level_text.setFont(font);
 	level_text.setCharacterSize(30);
 	level_text.setFillColor(Color::White);
-	level_text.setPosition(Vector2f(12.5f * cell_size, 5 * cell_size));
+	level_text.setPosition(Vector2f(12.5f * cell_size, 3 * cell_size));
 
 }
 
 void Board::draw(RenderTarget& target, RenderStates state) const {
 	target.draw(shape, state);
 }
-
 
 void Board::updateData(int _score) {
 	score = _score;
@@ -63,4 +62,9 @@ void Board::DrawRightPanel() {
 
 int Board::getLevel() {
 	return level;
+}
+
+void Board::DrawGameOver() {
+	gameover.setPosition(Vector2f(7.5f * cell_size, 10.f * cell_size));
+	window->draw(gameover);
 }
